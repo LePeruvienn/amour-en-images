0d14487858d9299ff0e0fbdb7098ed5ecb5d7cb8
LePeruvienn <koto91@free.fr>
2024-11-24 07:56:05 +0100

    update vite config

--- client/amour-en-images/vite.config.js
+++ client/amour-en-images/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: '/amour-en-images/',	// Spécifiez le sous-répertoire pour GitHub Pages
+	build: {
+		outDir: 'dist',	// Dossier de sortie de la construction
+		sourcemap: false,	// Désactive la génération des source maps pour la production
+	},
 })
